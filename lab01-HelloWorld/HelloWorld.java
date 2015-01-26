4af9d298e4e75a09be784b389ff6d4a5f23c896d
jhc417 <jhc417@lehigh.edu>
2015-01-26 01:54:07 +0000

    Lab 1

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//////////////////////////////
+// Jack Curran
+//Hello World Java Program
+
+
+//define a class
+public class HelloWorld{
+    //add a main method
+    public static void main(String[] args){
+    //print statement with border around it
+     System.out.println("######################");
+     System.out.println("######################");
+     System.out.println("#                    #");
+     System.out.println("# Hello World 2015!! #");
+     System.out.println("#                    #");
+     System.out.println("######################");
+     System.out.println("######################");
+     System.out.println("test");
+        
+        
+    }
+}
