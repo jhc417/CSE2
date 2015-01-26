d2e14974fd8884a2804bdd700deabf1d2ca09166
jhc417 <jhc417@lehigh.edu>
2015-01-26 23:39:12 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+/////////////////////////////////
+// Jack Curran
+// 1/26/2015
+// CSE2
+// hw01
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //type code for program
+        System.out.println("-------------------");
+        System.out.println("|     WELCOME     |");
+        System.out.println("-------------------");
+        System.out.println("  ^   ^   ^   ^   ^   ^");
+        System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\ ");
+        System.out.println("<-J- -H- -C- -4- -1- -7->");
+        System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ / ");
+        System.out.println("  v   v   v   v   v   v");
+        System.out.println("This summer my teammates and I will be spending a few months out in Boulder, Colorado. ");
+    }
+}
